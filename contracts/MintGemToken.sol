2ccfaf4aa41cc2281f22726481b5836609d77036
hyerin-ch <hrchun@creativehill.co.kr>
2022-07-20 03:00:57 +0900

    메타데이터 uri 적용

--- contracts/MintGemToken.sol
+++ contracts/MintGemToken.sol
@@ -4,20 +4,36 @@ pragma solidity >=0.7.0 <0.9.0;
 
 //erc721에서 util적인 기능이 추가된 타입
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol"; 
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MintGemToken is ERC721Enumerable {
 
+    string public metadataURI;
 
-    // 컨트랙트가 배포될때 최초로 실행됨
-    //erc721은 constructor를 실행할 때 기본적으로 플젝이름과 약자를 입력함
-    //constructor() ERC721("name", "symbol") {}
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+    constructor(string memory _name, string memory _symbol, string memory _metadataURI) ERC721(_name, _symbol) {
+        metadataURI = _metadataURI;
+    }
+
+    struct GemTokenData {
+        uint gemTokenRank;
+        uint gemTokenType;
+    }
+
+    mapping(uint => GemTokenData) public gemTokenData;
+
+    function tokenURI(uint _tokenId) override public view returns(string memory) {
+        string memory gemTokenRank = Strings.toString(gemTokenData[_tokenId].gemTokenRank);
+        string memory gemTokenType = Strings.toString(gemTokenData[_tokenId].gemTokenType);
+
+        return string(abi.encodePacked(metadataURI, '/', gemTokenType, '.json'));
+    }
+
 
     function mintGemToken() public  {
-        // 총 발행량을 리턴
         uint tokenId = totalSupply() + 1;
 
-        // nft의 발행자(함수를 실행하는 사람), 토큰id를 인자로 받는 mint함수
+        gemTokenData[tokenId] = GemTokenData(1,1);
+
         _mint(msg.sender, tokenId);
     }
 }
